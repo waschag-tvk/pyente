9ee492148e5e17a2602de5f1d421ea876c230f2f
Yushin Washio <yuwash@yandex.com>
2018-06-23 22:40:31 +0200

    remove menu bar

--- pyente/app.py
+++ pyente/app.py
@@ -23,54 +23,10 @@ class Ente(QMainWindow):
         self.widget = QWidget()
         self.layout = QHBoxLayout(self.widget)
 
-        self.menu_bar = self.menuBar()
         self.about_dialog = AboutDialog()
 
         self.status_bar = self.statusBar()
         self.status_bar.showMessage('Ready', 5000)
-
-        self.file_menu()
-        self.help_menu()
-
-        
-
-    def file_menu(self):
-        """Create a file submenu with an Open File item that opens a file dialog."""
-        self.file_sub_menu = self.menu_bar.addMenu('File')
-
-        self.open_action = QAction('Open File', self)
-        self.open_action.setStatusTip('Open a file into Ente.')
-        self.open_action.setShortcut('CTRL+O')
-        self.open_action.triggered.connect(self.open_file)
-
-        self.exit_action = QAction('Exit Application', self)
-        self.exit_action.setStatusTip('Exit the application.')
-        self.exit_action.setShortcut('CTRL+Q')
-        self.exit_action.triggered.connect(lambda: QApplication.quit())
-
-        self.file_sub_menu.addAction(self.open_action)
-        self.file_sub_menu.addAction(self.exit_action)
-
-    def help_menu(self):
-        """Create a help submenu with an About item tha opens an about dialog."""
-        self.help_sub_menu = self.menu_bar.addMenu('Help')
-
-        self.about_action = QAction('About', self)
-        self.about_action.setStatusTip('About the application.')
-        self.about_action.setShortcut('CTRL+H')
-        self.about_action.triggered.connect(lambda: self.about_dialog.exec_())
-
-        self.help_sub_menu.addAction(self.about_action)
-
-    
-
-    def open_file(self):
-        """Open a QFileDialog to allow the user to open a file into the application."""
-        filename, accepted = QFileDialog.getOpenFileName(self, 'Open File')
-
-        if accepted:
-            with open(filename) as file:
-                file.read()
 
 
 class AboutDialog(QDialog):
