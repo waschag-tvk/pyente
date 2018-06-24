bbaf275085432953a82b9dbc1fc586c223e0afd0
Yushin Washio <yuwash@yandex.com>
2018-06-24 02:45:00 +0200

    add control

---
+++ pyente/control.py
@@ -0,0 +1,14 @@
+import subprocess
+
+
+class Control:
+    def __init__(self, relaisctl_path='./relaisctl'):
+        self.relaisctl_path = relaisctl_path
+
+    def activate(self, machine):
+        status, output = subprocess.getstatusoutput(
+                '%s %d'.format(self.relaisctl_path, machine))
+        print('[relaisctl] ' + output)
+        if status != 0:
+            raise RuntimeError(
+                    'Activation failed with status {:d}!'.format(status))
